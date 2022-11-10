f7c6535dcc2d239b3cc2d9d86e01eced07bda87e
chinhcp <chinhcpdev@gmail.com>
2022-11-10 08:27:37 +0700

    landing-page -structure

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Landing from "./pages/Landing";
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <Landing />
+      </div>
+    </div>
+  );
+}
+
+export default App;
